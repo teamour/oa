1a974958d4db445f96f7420d9d6cbf226014128b
TigerHu33 <tigerhu33@gmail.com>
2019-04-21 18:05:20 +0900

    把主页的访问链接增加上了,测试方法:localhost:8080/user/login

--- oa/src/main/java/com/our/oa/controller/UserController.java
+++ oa/src/main/java/com/our/oa/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.our.oa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /*
  * 系统用户相关
@@ -11,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/user")
 public class UserController {
 
-	@PostMapping("/login")
-	public String login() {
+	@GetMapping("/login")
+	public ModelAndView login(ModelAndView modelAndView) {
 		// 登录操作.
 		
+		modelAndView.setViewName("home");
 		// 成功之后进入home页面
-		return "home";
+		return modelAndView;
 	}
 	
 	@PostMapping("/logout")
