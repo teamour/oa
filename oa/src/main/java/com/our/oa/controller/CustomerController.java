f4e2dd10263cf1c120dd36b3b35a740aef948d78
Aclaier <wang1jia2qin3@gmail.com>
2019-04-23 11:47:42 +0900

    搜索功能完成

--- oa/src/main/java/com/our/oa/controller/CustomerController.java
+++ oa/src/main/java/com/our/oa/controller/CustomerController.java
@@ -3,7 +3,7 @@ package com.our.oa.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.runner.ReactiveWebApplicationContextRunner;
+
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
