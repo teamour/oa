5a3e44ad371ed70ce566d4e43862ed5eee573b63
TigerHu33 <tigerhu33@gmail.com>
2019-04-23 18:33:52 +0900

    登陆功能添加

--- oa/src/main/resources/static/js/pages/emp/employeelist.js
+++ oa/src/main/resources/static/js/pages/emp/employeelist.js
@@ -4,15 +4,19 @@ BF={
 		             url:  ctxPath +'emp/list',
 		             mtype: "POST",
 		             datatype: "json",
-		             colNames:["Id", "标题", "创建时间", "内容"],
+		             colNames:["Id", "姓名", "工作年数", "日语等级","技术等级","工作1","工作2","工作3"],
 		             colModel: [
-		                 { input: 'employeeId', name: 'employeeId',hidden:true, width: 75 },
+		                 { label: 'employeeId', name: 'employeeId',hidden:true, width: 75 },
 		                 { label: 'employeeName', name: 'employeeName', width: 150 },
 		                 { label: 'createTime', name: 'createTime', width: 150,
 						formatter : 'date', formatoptions: { srcformat : 'Y-m-d H:i:s', newformat :'ShortDate'}},
-		                 { label: 'age', name: 'age', width: 150 }
+		                 { label: 'japaneseLevel', name: 'japaneseLevel', width: 150 },
+						{ label: 'skillLevel', name: 'skillLevel', width: 150 },
+						{ label: 'skill1Years', name: 'skill1Years', width: 150 },
+						{ label: 'skill2Years', name: 'skill2Years', width: 150 },
+						{ label: 'skill3Years', name: 'skill3Years', width: 150 }
 		             ],
-		             width: "800",
+		             width: "1000",
 		             height: "380",
 		             'loadError' : function (xhr, status, error){
 		                 alert(error);
@@ -20,7 +24,6 @@ BF={
 		             rownumber:true,
 		             viewrecords: true, 
 		             pager: '#jqGridPager',
-		             rowNum: 10,
 		             rowList: [5, 10, 50, 100, 500],
 		             multiselect: true,
 		             jsonReader:{  repeatitems:false },
@@ -33,22 +36,31 @@ BF={
 
 		},
 		modifyData:function(){
+			debugger
 			var selData = $("#jqGrid").getRowData();
 			// 要判断是否有选择，且只选中一行数据
+			var rowId=$("#jqGrid").jqGrid('getGridParam','selrow');
+			var id=selData[rowId-1].employeeId;
 			
-			
-			window.location.href = ctxPath+"emp/"+selData[0].employeeId;
+			window.location.href = ctxPath+"emp/"+id;
 		},
 		searchData: function (){
 			$("#jqGrid").setGridParam({
 	            page: 1,
-	            postData: { title :$("#title").val() }
+	            postData: { employeeId :$("#employeeId").val() }
 	        }).trigger('reloadGrid');
 	   },
 	   deleteByIds:function(){
 		   debugger
-		   var ids = $("#jqGrid").jqGrid("getGridParam","selarrrow");
-		   
+		   var selData = $("#jqGrid").getRowData();//获取行数rows
+		   var ids_t = $("#jqGrid").jqGrid("getGridParam","selarrrow");//获得当前页其中的那几行
+		   var ids = new Array();
+		   var len = ids_t.length;
+		   var a;
+		   for(var i=0;i<len;i++){
+			   a = ids_t[i]
+			   ids[i] = selData[a-1].employeeId;
+		   }
 		   if(!confirm("您确认删除吗"))return;
 		   //1.定义请求的url
 		   var url="http://localhost:8080/emp/deleteByIds";
@@ -59,8 +71,9 @@ BF={
 		   }
 		   var params={"ids":ids.toString()};//ids=1,2,3,4
 		   //3.执行异步删除操作
-		   $.post(url,params,function(result){});
-		   
+		   $.post(url,params,function(result){
+			   location.href="http://localhost:8080/emp/list";
+		   });
 		   
 	   }
 }
