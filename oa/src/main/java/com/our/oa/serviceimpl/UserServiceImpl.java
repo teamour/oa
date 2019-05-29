936eb49a28e01fd7c72c5fc5d5f60cf94ffe7545
chiqing151 <49706600+chiqing151@users.noreply.github.com>
2019-05-29 20:51:09 +0900

    Revert "同步版本"
    This reverts commit 8a0f7b9bc5fce2ffaff7c7f3919c751b1f41ccee.

--- oa/src/main/java/com/our/oa/serviceimpl/UserServiceImpl.java
+++ oa/src/main/java/com/our/oa/serviceimpl/UserServiceImpl.java
@@ -1,6 +1,12 @@
 package com.our.oa.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -11,20 +17,20 @@ import com.our.oa.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
-//	 @Autowired
-//	    private UserMapper userMapper;
-//	 
-//	 
-//	    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//	        User user = userMapper.queryByUserName(username);
-//	        System.out.println("User : "+user.getUserName());
-//	        if(user==null){
-//	            System.out.println("User not found");
-//	            throw new UsernameNotFoundException("Username not found");
-//	        }else {
-//	return new  org.springframework.security.core.userdetails.User(username, username, false, false, false, false, null);}
-//	        
-//	    }
+	 @Autowired
+	    private UserMapper userMapper;
+	 
+	 
+	    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	        User user = userMapper.queryByUserName(username);
+	        System.out.println("User : "+user.getUserName());
+	        if(user==null){
+	            System.out.println("User not found");
+	            throw new UsernameNotFoundException("Username not found");
+	        }else {
+	        	return new  org.springframework.security.core.userdetails.User(username, username, false, false, false, false, null);}
+	        
+	    }
 	 
 
 }
